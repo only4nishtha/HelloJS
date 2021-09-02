6b682b518d595546ff4ccbdb6cc71ad3934a004f
only4nishtha <only4nishtha@gmail.com>
2021-09-02 19:30:54 +0530

    used Console Operations

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hey, this is my first piece of JavaScript code!!! WOHOO")
+console.log({name: "Nishtha" })
+console.log(true)
+console.log(false)
+console.log(false, 12345, 'string', { name: "Nishtha"})
+console.warn("This is a WARNING")
+console.error("this is an error")
+console.log('clear')
