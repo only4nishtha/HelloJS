aa98521df3ee679159ffa745d571aed9fa56f916
only4nishtha <only4nishtha@gmail.com>
2021-09-03 20:04:23 +0530

    Comparison Operators

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-// Assignment Operators
+// Comparison Operators
 
-let Name = "Nishtha"
-let a = 19
-console.log(typeof a)
-a += Name
-console.log(typeof a)
-console.log(a)
+const a = 21
+const b = 7
+const c = 21
+console.log('a > b', a > b)
+console.log('a < b', a < b)
+console.log('a == b', a == b)
+console.log('a == c', a == c)
+console.log('a >= c', a >= c)
 
-// a/=7
-// a*=
-// a**= FOR EXPONENTIAL FUNCTION
+console.log("________________________________")
+
+const e = 'Nishtha'
+const d = 'nishtha'
+console.log('e == d', e == d)
+console.log('e === d', e === d) //STRICT OPERATOR- compares data type too
