f747bf3ec8dba3e6d0c2c395b3385f2a4a042aa8
only4nishtha <only4nishtha@gmail.com>
2021-09-04 20:00:16 +0530

    Event Selectors

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-const element = document.querySelector('#selectme')
-// element.innerHTML = "This is from JS"
-// console.log(element)
+const button = document.querySelector('button')
+const element = document.querySelector('body')
 
-// element.style.color = "cyan"
-// element.style.fontSize = "40px"
-// element.style = "display:none"
-
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// element.classList.remove('one')
-// console.log(element.classList)
-
-// const body = document.querySelector('body')
-// console.log(body.classList.toggle('dark'))
-
-const value = element.classList.contains('five')
-console.log(value)
-
-const values = element.classList.contains('three')
-console.log(values)
+button.addEventListener('click', () => {
+    // console.log("U clicked me")
+    element.classList.toggle('dark')    
+})
