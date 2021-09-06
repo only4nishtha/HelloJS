738338350251d91a9c5ae1650ae558e744d258a8
only4nishtha <only4nishtha@gmail.com>
2021-09-06 19:32:18 +0530

    Rock Paper Scissors

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-// const rock = document.querySelector('#rock')
-// const paper = document.querySelector('#paper')
-// const scissor = document.querySelector('#scissor')
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector('#board')
 
-const add1 = (a=0, b=0) => { //this is preferred since all functions are declared iniitially and it looks neat
-    return a + b
+const handler = (event) => {
+    const action = event.target.id
+    // if(action === 'rock'){
+    //     board.innerHTML = "Rock crushes Scissor"
+    // }
+    // else if(action === 'paper'){
+    //     board.innerHTML = "Paper traps Rock"
+    // }
+    // else if(action === 'scissor'){
+    //     board.innerHTML = "Scissor cuts Paper"
+    // }
+    // else{
+    //     board.innerHTML = "Click Something"
+    // }
+    switch (action) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+        default:
+            board.innerHTML = "Click something"
+    }
+
 }
-console.log(add1(12, 23))
 
-console.log(add2(12, 5)) //with FUNCTION KEYWORD, this line can be used before declaration.
-
-function add2(a=0, b=0){
-    return a+b
-}
-
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
