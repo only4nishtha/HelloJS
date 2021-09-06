82be1dcb9f44b2a966fdf05d183ee235e7deb1a8
only4nishtha <only4nishtha@gmail.com>
2021-09-06 18:25:56 +0530

    Function Keywords

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const button = document.querySelector('button')
-const element = document.querySelector('body')
+// const rock = document.querySelector('#rock')
+// const paper = document.querySelector('#paper')
+// const scissor = document.querySelector('#scissor')
 
-button.addEventListener('click', () => {
-    // console.log("U clicked me")
-    element.classList.toggle('dark')    
-})
+function logName(Name="Enter your name", age = 18){
+    console.log(Name + " You are" + age)
+}
+
+logName() //output UNDEFINED if no parameter is passed
+
+function add(a=0, b=0){
+    const sum= a+b
+    return sum
+}
+console.log(add())
